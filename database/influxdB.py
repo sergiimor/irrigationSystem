97fe075d2646e6063b29e425a66127e41a4363f5
ilerda <ilerda@users.noreply.github.com>
2020-05-01 17:49:25 +0200

    aligne Raspberry

--- database/influxdB.py
+++ database/influxdB.py
@@ -1,1 +1,92 @@
-import time
+import datetime
+import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
+import main as pr
+
+idb = InfluxDBClient(database="sensors")
+
+jsondata = [{
+    "measurement": "",
+    "time": 0,
+    "fields":
+        {
+            "value": 0
+        }
+}, {
+    "measurement": "",
+    "time": 0,
+    "fields":
+        {
+            "value": 0
+        }
+}, {
+    "measurement": "",
+    "time": 0,
+    "fields":
+        {
+            "value": 0
+        }
+}, {
+    "measurement": "",
+    "time": 0,
+    "fields":
+        {
+            "value": 0
+        }
+}, {
+    "measurement": "",
+    "time": 0,
+    "fields":
+        {
+            "value": 0
+        }
+}, {
+    "measurement": "",
+    "time": 0,
+    "fields":
+        {
+            "value": 0
+        }
+}, ]
+
+sensor_tm = str(pr.Temperatura("Temperatura"))
+a, temperatura = sensor_tm.split(":")
+print(temperatura)
+
+sensor_hm = str(pr.Humidity("Humitat"))
+a, humitat = sensor_hm.split(":")
+
+def connecta(client, userdata, flags, rc):
+    print
+    "Connected: " + str(rc)
+    client.subscribe("sensor")
+
+
+def missatge(client, userdata, msg):
+    global sensor
+    sensor = str(msg.payload)
+    print
+    "rebut: ", sensor
+    mesurement = ["temperatura", "humitat", "ldr", "vent", "pluja"]
+
+    jsondata[0]["time"] = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+    jsondata[0]["measurement"] = "temperatura"
+    jsondata[0]["fields"]["value"] = temperatura
+
+    jsondata[1]["time"] = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+    jsondata[1]["measurement"] = "humitat"
+    jsondata[1]["fields"]["value"] = humitat
+
+    print
+    jsondata
+    idb.write_points(jsondata)
+
+
+client = mqtt.Client()
+client.on_connect = connecta
+client.on_message = missatge
+
+client.connect("127.0.0.1", 3306, 60)
+
+while True:
+    client.loop()
--- main.py
+++ main.py
@@ -1,5 +1,60 @@
+import paho.mqtt.publish as publish
 import time
 import random
+#import Adafruit_DHT
+#from datetime import datetime, date, time, timedelta
+#import calendar
+#import RPi.GPIO as GPIO, time,os
+# import spidev
+#from gpiozero import MCP3008
 
+#spi=spidev.SpiDev()
+#spi.open(0,0)
+
+class Sensor(object):
+
+    def __init__(self, name="SensorGeneric"):
+        self.name = name
+
+    def read_data(self):
+        return None
+
+    def read_spi_channel(self, channel):
+        #       spidata=spi.xfer2([1,(8+channel)>>4,0])
+        #data = (((spidata[1] %3 )<< 8 )+ spidata[2])
+        #return data
+        return None
+    def __str__(self):
+        return self.name+":{}".format(self.read_data())
+
+
+class Temperatura(Sensor):
+
+    def __init__(self, name):
+        Sensor.__init__(self, name)
+
+    def read_data(self):
+        #humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,4)
+        temperature = random.randint(0, 43)
+
+        return temperature
+
+class Humidity(Sensor):
+
+    def __init__(self, name):
+        Sensor.__init__(self, name)
+
+    def read_data(self):
+        #humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,4)
+        humidity = random.randint(0,100)
+        return humidity
 
 if __name__ == "__main__":
+    while True:
+        tm = Temperatura("Temperatura")
+        publish.single("sensors", tm, hostname="127.0.0.1")
+        print (tm)
+        hm = Humidity ("Humitat")
+        print (hm)
+        publish.single("sensors", hm, hostname="127.0.0.1")
+        time.sleep(5)
