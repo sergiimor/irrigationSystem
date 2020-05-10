f8b7ce3f27cacfcc2d2e45eff08e433026bea144
ilerda <ilerda@users.noreply.github.com>
2020-05-10 21:27:07 +0200

    test3

--- notification.py
+++ notification.py
@@ -13,7 +13,9 @@ now = datetime.datetime.now() # Getting date and time
 
 GPIO.setmode(GPIO.BCM)      # Use Board pin numbering
 GPIO.setup(releCtrl, GPIO.OUT) # Declaring the GPIO 21 as output pin
+bot = telepot.Bot('1075608346:AAGw8vhREx5WAMmV3aQpclk5yKjYWzn9qiI')
 
+print (bot.getMe())
 
 def handle(msg):
     chat_id = msg['chat']['id'] # Receiving the message from telegram
@@ -42,15 +44,14 @@ def handle(msg):
 			bot.sendMessage(chat_id, str("Cerrando "))
 			GPIO.output(releCtrl, True)
 			changeReleState (releStatus)
-			print "is releStatus is true" + releStatus
+			print "is releSatatus is true" + releStatus
 		else:
 			bot.sendMessage(chat_id, str("Abriendo "))
 			GPIO.output(releCtrl, True)
 			changeReleState (releStatus)
 			print "is not releStatus is true" + releStatus
 # Insert your telegram token below
-bot = telepot.Bot('1075608346:AAGw8vhREx5WAMmV3aQpclk5yKjYWzn9qiI')
-print (bot.getMe())
+
 def relehandler(releStatus):
 	releStatus != releStatus
 	return releStatus
@@ -60,9 +61,57 @@ def changeReleState(releStatus):
 	else:
 		releStatus = 0
 	return releStatus
+	
+def start(update, context):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def button(update, context):
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    query.edit_message_text(text="Selected option: {}".format(query.data))
+
+
+def help(update, context):
+    update.message.reply_text("Use /start to test this bot.")
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 # Start listening to the telegram bot and whenever a message is  received, the handle function will be called.
 MessageLoop(bot, handle).run_as_thread()
 print ('Listening....')
 
-while 1:
-    sleep(10)
+def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("TOKEN", use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
